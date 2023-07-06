702e5ee57c43750623b788f786b8fdc6c10f596b
Sasha Dyachkov <sanya7765045@gmail.com>
2023-07-06 18:00:26 +0300

    add GetArray function

---
+++ Programm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+    static string[] GetArray()
+    {
+        // Запросить у пользователя размер массива
+        Write("Введите размер массива: ");
+        int n = int.Parse(ReadLine());
+
+        // Создать массив заданного размера
+        string[] array = new string[n];
+
+        // Запросить у пользователя ввод символов или строк и добавить их в массив
+        for (int i = 0; i < n; i++)
+        {
+            Write($"Введите символ или строку {i + 1}: ");
+            string input = ReadLine();
+            array[i] = input;
+        }
+
+        // Вывести массив
+        WriteLine("Массив элементов: " + string.Join(", ", array));
+        return array;
+    }
+
+        // Очищаем консоль, чтобы пользователь мог вводить данные
+        Clear();
+
+        string[] myArray = GetArray();
+
+        // Ожидание нажатия клавиши, чтобы консоль не закрылась сразу
+        ReadKey();
+
